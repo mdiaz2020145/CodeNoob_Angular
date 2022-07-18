cf9c818cb60c023ad3b0bdef5a6e1b450c1e9c2a
Marcelo Diaz <88795578+mdiaz2020145@users.noreply.github.com>
2022-07-17 21:21:41 -0600

    Bienvenida y navbar

--- CodeNoob/src/app/app-routing.module.ts
+++ CodeNoob/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BienvenidaComponent } from './components/bienvenida/bienvenida.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"bienvenida",component:BienvenidaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- CodeNoob/src/app/app.module.ts
+++ CodeNoob/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BienvenidaComponent } from './components/bienvenida/bienvenida.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BienvenidaComponent
   ],
   imports: [
     BrowserModule,
